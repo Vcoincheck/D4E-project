4b5d1cae21aef6431568881545babbf718218d3b
VCC labs <131963795+Vcoincheck@users.noreply.github.com>
2025-10-18 07:34:08 +0000

    chore: commit remaining import/path fixes after restructure

--- src/components/dao/CreateDAOWizard.tsx
+++ src/components/dao/CreateDAOWizard.tsx
@@ -4,12 +4,12 @@ import { useLanguage } from '../providers/LanguageProvider';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
 import { Check, ChevronLeft, ChevronRight } from 'lucide-react';
 import { cn } from '../ui/utils';
-import { DAOInfoStep } from '../../wizard/steps/DAOInfoStep';
-import { GovernanceSetupStep } from '../../wizard/steps/GovernanceSetupStep';
-import { VotingMethodStep } from '../../wizard/steps/VotingMethodStep';
-import { TreasuryMultisigStep } from '../../wizard/steps/TreasuryMultisigStep';
-import { AddMembersStep } from '../../wizard/steps/AddMembersStep';
-import { ReviewConfirmStep } from '../../wizard/steps/ReviewConfirmStep';
+import { DAOInfoStep } from '../wizard/steps/DAOInfoStep';
+import { GovernanceSetupStep } from '../wizard/steps/GovernanceSetupStep';
+import { VotingMethodStep } from '../wizard/steps/VotingMethodStep';
+import { TreasuryMultisigStep } from '../wizard/steps/TreasuryMultisigStep';
+import { AddMembersStep } from '../wizard/steps/AddMembersStep';
+import { ReviewConfirmStep } from '../wizard/steps/ReviewConfirmStep';
 
 
 
